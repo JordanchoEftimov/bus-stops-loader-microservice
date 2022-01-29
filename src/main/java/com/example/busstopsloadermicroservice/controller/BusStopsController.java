19c9af8587fcefb834e08aab44b176cddc7f3bb8
Jordancho Eftimov <75941337+JordanchoEftimov@users.noreply.github.com>
2022-01-29 20:11:14 +0100

    Cross Origin enabled

--- src/main/java/com/example/busstopsloadermicroservice/controller/BusStopsController.java
+++ src/main/java/com/example/busstopsloadermicroservice/controller/BusStopsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/busStops")
 public class BusStopsController {
     private final BusStopsService busStopsService;
