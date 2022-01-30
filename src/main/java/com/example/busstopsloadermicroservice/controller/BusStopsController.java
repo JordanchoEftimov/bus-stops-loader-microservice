5e3a0b2f8d483ceb65316530a1cf7405fa39cedd
Jordancbo Eftimov <ejordancho@gmail.com>
2022-01-30 13:43:01 +0100

    Added additional info about bus stops in api response.

--- src/main/java/com/example/busstopsloadermicroservice/controller/BusStopsController.java
+++ src/main/java/com/example/busstopsloadermicroservice/controller/BusStopsController.java
@@ -1,35 +1,64 @@
 package com.example.busstopsloadermicroservice.controller;
 
 import com.example.busstopsloadermicroservice.model.BusStops;
-import com.example.busstopsloadermicroservice.service.BusStopsService;
-import org.springframework.http.HttpHeaders;
+import com.example.busstopsloadermicroservice.repository.BusStopsRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/busStops")
 public class BusStopsController {
-    private final BusStopsService busStopsService;
+    private final BusStopsRepository busStopsRepository;
 
-    public BusStopsController(BusStopsService busStopsService) {
-        this.busStopsService = busStopsService;
+    public BusStopsController(BusStopsRepository busStopsRepository) {
+        this.busStopsRepository = busStopsRepository;
     }
 
     @GetMapping
-    public ResponseEntity<List<BusStops>> getBusses(
+    public ResponseEntity<Map<String, Object>> getBusses(
             @RequestParam(required = false) String query,
             @RequestParam(defaultValue = "0") Integer page,
             @RequestParam(defaultValue = "9") Integer pageSize,
             @RequestParam(defaultValue = "number") String sortBy) {
-        List<BusStops> list = busStopsService.getBusses(query, page, pageSize, sortBy);
-        return new ResponseEntity<>(list, new HttpHeaders(), HttpStatus.OK);
+        try {
+            List<BusStops> busses;
+            Pageable paging = PageRequest.of(page, pageSize, Sort.by(sortBy));
+
+            Page<BusStops> busStopsPage;
+            if (query == null)
+                busStopsPage = busStopsRepository.findAll(paging);
+            else
+                busStopsPage = busStopsRepository.findAllByQuery(query, paging);
+
+            busses = busStopsPage.getContent();
+            boolean hasNext = busStopsPage.getNumber() < busStopsPage.getTotalPages() - 1;
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("busses", busses);
+            response.put("currentPage", busStopsPage.getNumber());
+            response.put("totalItems", busStopsPage.getTotalElements());
+            response.put("totalPages", busStopsPage.getTotalPages());
+            response.put("hasNext", hasNext);
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
 }
+
--- src/main/java/com/example/busstopsloadermicroservice/service/BusStopsService.java
+++
@@ -1,9 +0,0 @@
-package com.example.busstopsloadermicroservice.service;
-
-import com.example.busstopsloadermicroservice.model.BusStops;
-
-import java.util.List;
-
-public interface BusStopsService {
-    List<BusStops> getBusses(String query, Integer page, Integer pageSize, String sortBy);
-}
